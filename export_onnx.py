0e53f1c81c266705b6cecb52eaa54a4f14c45095
huangbo <cenahwang0304@gmail.com>
2024-09-13 21:00:33 +0800

    first commit

---
+++ export_onnx.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File      : export_onnx.py
+@Time      : 2024/09/13 20:40:50
+@Author    : Huang Bo
+@Contact   : cenahwang0304@gmail.com
+@Desc      : None
+'''
+
+'''
+/root/TensorRT-8.5.1.7/targets/x86_64-linux-gnu/bin/trtexec --onnx=RMBG_1.4.opt.onnx --minShapes=img:1x3x256x256 --optShapes=img:3x3x1024x1024 --maxShapes=img:5x3x1024x1024 --fp16 --saveEngine=RMBG_1.4.opt.plan
+'''
+
+import torch
+import onnx
+from onnxsim import simplify
+from pprint import pprint
+import onnxruntime
+
+from pytorch_inference import BriaRMBGPipe
+
+
+def check_model(onnx_path, onnx_opt_path):
+    model = onnx.load(onnx_path)
+    simplified_model, check = simplify(model)
+    assert check, "Simplified ONNX model could not be validated"
+    onnx.save(simplified_model, onnx_opt_path)
+    try:
+        onnx.checker.check_model(onnx_opt_path)
+        print("model check passed!")
+    except Exception as e:
+        print(f"model check failed: {e}")
+        
+def print_in_out_name(onnx_opt_path):
+    onnx_model = onnx.load(onnx_opt_path)
+    onnx_session = onnxruntime.InferenceSession(onnx_model.SerializeToString())
+
+    print("------input-------")
+    input_tensors = onnx_session.get_inputs()
+    for input_tensor in input_tensors:
+        input_info = {
+            "name": input_tensor.name,
+            "type": input_tensor.type,
+            "shape": input_tensor.shape,
+        }
+        pprint(input_info)
+        
+    print("------output-------")
+    output_tensors = onnx_session.get_outputs()
+    for output_tensor in output_tensors:
+        output_info = {
+            "name" : output_tensor.name,
+            "type" : output_tensor.type,
+            "shape": output_tensor.shape,
+        }
+        pprint(output_info)
+        
+        
+        
+if __name__ == "__main__":
+    rmbg = BriaRMBGPipe("RMBG-1.4") # hugging face
+    fp16 = False
+    
+    # export to onnx
+    onnx_path = 'RMBG_1.4.onnx'
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    img_tensor = torch.randint(0, 256, (1, 3, 1024, 1024), dtype=torch.uint8).to(device)
+    onnx_opt_path = onnx_path.replace(".onnx", ".opt.onnx")
+    
+    torch.onnx.export(
+        rmbg.half() if fp16 else rmbg,
+        img_tensor,
+        onnx_path,
+        opset_version=17,
+        input_names=['img'],
+        output_names=['mask'],
+        dynamic_axes={"img": {0: "bs", 2: "h", 3: "w"},
+                      "mask": {0: "bs", 2: "h", 3: "w"},
+                     },
+    )
+    check_model(onnx_path, onnx_opt_path)
+    print_in_out_name(onnx_opt_path)
---
+++ pytorch_inference.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File      : pytorch_inference.py
+@Time      : 2024/09/13 20:01:27
+@Author    : Huang Bo
+@Contact   : cenahwang0304@gmail.com
+@Desc      : None
+'''
+
+
+'''
+source code borrow from: https://huggingface.co/spaces/briaai/BRIA-RMBG-1.4/blob/main/app.py
+'''
+
+import os
+import cv2
+import time
+from glob import glob
+from tqdm import tqdm
+import numpy as np
+import torch
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+
+from briarmbg import BriaRMBG
+
+
+class BriaRMBGPipe(nn.Module):
+    def __init__(self, model_path: str="briaai/RMBG-1.4", gpu_id: int=0):
+        super().__init__()
+        self.model = BriaRMBG.from_pretrained(model_path)
+        self.device = torch.device(f"cuda:{str(gpu_id)}" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        self.model.eval()
+        self.normalize_mean = torch.tensor([0.5, 0.5, 0.5], device=self.device).view(1, 3, 1, 1)
+        self.normalize_std = torch.tensor([1.0, 1.0, 1.0], device=self.device).view(1, 3, 1, 1)
+        
+    def preprocess(self, img_rgb: Tensor):
+        img_rgb = img_rgb.to(self.device)
+        img_rgb = img_rgb.float() / 255.0
+        img_resized = F.interpolate(img_rgb, size=[1024, 1024], mode='bilinear', align_corners=False)
+        img_normalized = (img_resized - self.normalize_mean) / self.normalize_std
+        
+        return img_normalized
+
+    @torch.no_grad()
+    def forward(self, img_rgb: Tensor):
+        size = img_rgb.shape[2:]
+        img_tensor = self.preprocess(img_rgb)
+        preds = self.model(img_tensor)
+        preds = torch.squeeze(F.interpolate(preds[0][0], size=tuple(size), mode='bilinear'), 0)
+        ma = torch.max(preds)
+        mi = torch.min(preds)
+        mask = (preds-mi)/(ma-mi)
+        # mask = (mask*255).to(torch.uint8)
+
+        return mask
+
+
+if __name__ == "__main__":
+    rmbg = BriaRMBGPipe("RMBG-1.4") # hugging face
+    
+    save_root = "result"
+    os.makedirs(save_root, exist_ok=True)
+    img_paths = glob(os.path.join("data", "*.jpg"))
+
+    for img_path in tqdm(img_paths):
+        img_name = os.path.basename(img_path).split(".")[0]
+        img = cv2.imread(img_path)
+        img_tensor = torch.from_numpy(img).unsqueeze(0)[..., (2, 1, 0)].permute(0, 3, 1, 2)  # bgr -> rgb
+        # print(img_tensor.shape)
+        size = tuple(img_tensor.shape[2:])
+        
+        # inference
+        t0 = time.time()
+        preds = rmbg(img_tensor)
+        print(f"inference use time: {(time.time()-t0):.3f} s")
+        
+        # print(preds.shape)
+        mask = preds.permute(1, 2, 0).cpu().numpy()
+        # print(mask.shape)
+        
+        # save as 4 channel image
+        result = np.zeros((size[0], size[1]*2, 4))
+        result[:, :size[1], :3] = img
+        result[:, :size[1], 3] = np.ones(size) * 255
+        result[:, size[1]:, :3] = img
+        result[:, size[1]:, 3] = mask[:, :, 0] * 255.0
+        cv2.imwrite(os.path.join(save_root, f"{img_name}_matting.png"), result)
+
+        # save as 3 channel image
+        result = img * mask
+        cv2.imwrite(os.path.join(save_root, f"{img_name}_result.png"), result)
